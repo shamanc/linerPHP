6b5248a6c1a4409ed25f2b849b5afb080bb06b5a
shamanc <72357786+shamanc@users.noreply.github.com>
2024-02-15 11:48:13 +0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,4 @@
+window.ec = window.ec || Object();
+  window.ec.storefront = window.ec.storefront || Object();
+  window.ec.storefront.product_details_show_product_name = false;
+  Ecwid.refreshConfig();
