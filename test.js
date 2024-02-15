aba76b5cd6a4385201052329e1da748613fe2d67
shamanc <72357786+shamanc@users.noreply.github.com>
2024-02-15 11:58:13 +0400

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,8 @@ window.ec = window.ec || Object();
   window.ec.storefront = window.ec.storefront || Object();
   window.ec.storefront.product_details_show_product_name = false;
   Ecwid.refreshConfig();
+
+window.ec = window.ec || Object();
+  window.ec.storefront = window.ec.storefront || Object();
+  window.ec.storefront.product_details_layout = "THREE_COLUMNS_SIDEBAR_ON_THE_RIGHT"; // set product page layout
+  Ecwid.refreshConfig();
